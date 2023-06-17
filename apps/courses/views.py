bb97ee2370fb34221bae8fd8645c428efd9baee4
zhanyl <kasymkulovajanylai@gmail.com>
2023-06-17 17:05:08 +0600

    create category

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from apps.courses.serializers import CategorySerializer
+from apps.courses.models import Category
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailView(generics.RetrieveAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
